bd2e2ed5d26c41d5955db527a36a5c6d58145390
Antonin Delpeuch <antonin@delpeuch.eu>
2012-10-20 23:04:19 +0200

    Intégration du code dans tape.ml avec le reste.

--- main.ml
+++ main.ml
@@ -1,7 +1,17 @@
 
+open Tape
+open Netlist_ast
 
-let pr = Netlist.read_file "test/fulladder.net" in
-Scheduler.schedule_program pr;
+let inputs = [| false; false; false; false; false; true; false; true; false; false; true; true; true; false; false; true; false; true; true; true; false; true; true; true |] 
+
+let pr = Netlist.read_file "test/fulladder.net" 
+let proxy = Netlist_proxy.create_from_program pr
+ 
+let outputs = simulate proxy pr.p_eqs 8 (Array.map (fun x -> { v = [| x |]; a =
+    fun () -> () }) inputs) 
+
+let () = List.iter (fun x -> Format.printf "%b " x) outputs;
+print_newline ()
 
 
 
--- scheduler.ml
+++ scheduler.ml
@@ -6,47 +6,13 @@
 
 open Netlist_ast
 
-type iarg =
-    | IAvar of int
-    | IAconst of value
-
-type iexp =
-    | IEarg of iarg
-    | IEreg of int 
-    | IEnot of iarg
-    | IEbinop of binop * iarg * iarg
-    | IEmux of iarg * iarg * iarg
-    | IErom of int * int * iarg
-      (*addr size, word size, read addr*)
-    | IEram of int * int * iarg * iarg * iarg * iarg
-      (*addr size, word size, read addr, write enable, write addr, data*)
-    | IEconcat of iarg * iarg
-    | IEslice of int * int * iarg
-    | IEselect of int * iarg
-
-type iequation = int * iexp
-
 (*
  * Traduction de la netlist originale en netlist ordonnée avec identifiants
  * entiers
  *)
 
-(*
- * Étant donné une (int * 'a) list (data) et une int list (order)
- * renvoyer la liste data réordonnée pour que les étiquettes soient dans le même 
- * ordre que dans 'order'.
- *)
-let reorder data order =
-    let max_val = List.fold_left max 1 order in
-    let comp = Array.make (max_val + 1) 0 in
-    let _ = List.fold_left (fun i v -> comp.(v) <- i; (i+1)) 0 order in
-    
-    List.sort (fun (xa,va) (xb,vb) -> comp.(xa) - comp.(xb)) data
 
-
-
-let schedule_program pr =
-    let proxy = Netlist_proxy.create_from_program pr in
+let schedule_program proxy =
     let nb_noeuds = Netlist_proxy.nb_identifiers proxy in
     let gr = Graph.make nb_noeuds in
 
@@ -57,42 +23,6 @@ let schedule_program pr =
 
     let order = List.rev (Graph.tsort gr) in
 
-    let get_id = Netlist_proxy.get_id proxy in
-
-    let translate_arg = function
-        | Aconst(x) -> IAconst(x)
-        | Avar(s) -> IAvar(get_id s)
-    in
-
-    let translate_expr = function
-        | Earg(a) -> IEarg(translate_arg a)
-        | Ereg(str) -> IEreg(get_id str)
-        | Enot(arg) -> IEnot(translate_arg arg)
-        | Ebinop(op,a,b) -> IEbinop(op, translate_arg a, translate_arg b)
-        | Emux(a,b,c) -> IEmux(translate_arg a,translate_arg b,translate_arg c)
-        | Erom(a,b,c) -> IErom(a, b, translate_arg c)
-        | Eram(a,b,c,d,e,f) -> IEram(a,b,translate_arg c, translate_arg d,
-                                    translate_arg e, translate_arg f)
-        | Econcat(a,b) -> IEconcat(translate_arg a, translate_arg b)
-        | Eslice(a,b,c) -> IEslice(a, b, translate_arg c)
-        | Eselect(a,b) -> IEselect(a, translate_arg b)
-    in
-
-    let translate_eqn (name,expr) =
-        (get_id name,translate_expr expr)
-    in
-
-    let translated = List.map translate_eqn pr.p_eqs in
-
-    (* Réorganise la liste des instructions selon l'ordre défini par 'order' *)
-    let reordered = reorder translated order in
-
-    (* debug temporaire : *)
-    List.iter (fun i ->
-        Format.printf "[%d] : %s\n" i (Netlist_proxy.get_name proxy i))
-    order;
-
-    reordered
-
+    order
 
 
