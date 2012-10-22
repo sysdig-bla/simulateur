a3541fd780db06c696e1220b783c53314389a7f2
Li-yao Xia <li-yao.xia@ens.fr>
2012-10-22 23:31:01 +0200

    Gestion des RAM/ROM. L'initialisation est encore à implémenter mais sinon ça semble fonctionner.

--- tape.ml
+++ tape.ml
@@ -192,16 +192,6 @@ let ebinop v1 v2 = function
   | And -> ebinop_aux v1 v2 (fun b1 b2 -> b1 && b2)
   | Nand -> ebinop_aux v1 v2 (fun b1 b2 -> not (b1 && b2))
 
-let rec read_memory r i l w = match w / l with
-  | 0 -> Array.sub r.(i) 0 w
-  | _ -> Array.append r.(i) (read_memory r (i + 1) l (w - l))
-
-let rec write_memory r v1 i l w = match w / l with
-  | 0 -> Array.blit v1 0 r.(i) 0 w
-  | _ -> Array.blit v1 0 r.(i) 0 l;
-    let v2 = Array.sub v1 l ((Array.length v1) - l - 1) in
-    write_memory r v2 (i + 1) l (w - l)
-
 (* Sets the value of a variable, in the right field according to the
  * current_value_in_v0 flag. *)
 let setv t i v =
@@ -209,6 +199,12 @@ let setv t i v =
         t.(i).v0 <- v
     else
         t.(i).v1 <- v
+
+let rec write_memory r v1 i l w = match w / l with
+  | 0 -> Array.blit v1 0 r.(i) 0 w
+  | _ -> Array.blit v1 0 r.(i) 0 l;
+    let v2 = Array.sub v1 l ((Array.length v1) - l - 1) in
+    write_memory r v2 (i + 1) l (w - l)
 
 let interpret t rom ram i1 = function
   | Earg a -> fun () ->
@@ -228,16 +224,16 @@ let interpret t rom ram i1 = function
     if v1.(0)
     then setv t i1 (evalue t a2)
     else setv t i1 (evalue t a3)
-  | Erom (l, w, a) -> fun () -> 
-    let i2 = int_of_argument t a in
-    setv t i1 (read_memory rom i2 l w)
-  | Eram (l, w, a1, a2, a3, a4) -> fun () ->
-    let i2 = int_of_argument t a1 in
-    let i3 = int_of_argument t a3 in
-    let v = evalue t a4 in
-    let () = setv t i1 (read_memory ram i2 l w) in
-    if bool_of_argument t a2
-    then write_memory ram v i3 l w
+  | Erom (l, w, a) ->
+    let r = Array.init l (fun _ -> Array.make w false) in
+    fun () -> setv t i1 r.(int_of_argument t a)
+  | Eram (l, w, ra, we, wa, d) -> 
+    let r = Array.init l (fun _ -> Array.make w false) in
+    fun () -> begin
+      setv t i1 r.(int_of_argument t ra);
+      if bool_of_argument t we
+        then r.(int_of_argument t wa) <- evalue t d
+    end
   | Econcat (a1, a2) -> fun () ->
     let v1 = evalue t a1 in
     let v2 = evalue t a2 in
